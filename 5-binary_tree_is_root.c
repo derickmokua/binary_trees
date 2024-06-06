e7792bec87af646fa5ddadfd9467914f7e8551fa
derick mokua <derickmokua@outlook.com>
2024-06-06 11:46:29 -0700

    is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,8 +9,10 @@
  * 1 if the node is a root
  * 0 if not a root or if the node is NULL
  */
-
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	return ((!node || node->parent) ? 0 : 1);
+	if (node == NULL || node->parent != NULL)
+		return (0);
+
+	return (1);
 }
