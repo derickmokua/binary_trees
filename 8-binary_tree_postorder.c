22796236b95a132744ff657b2a44ce8a5b5d01a5
derick mokua <derickmokua@outlook.com>
2024-06-06 11:26:40 -0700

    8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,13 +6,12 @@
  * @tree: Tree to traverse
  * @func: Pointer to a function to call for each node
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
-
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
